78435a953c303828427a74fcd4c3dd30a89d8fed
william2271 <william22711@gmail.com>
2025-08-07 20:22:40 -0400

    Initial commit

---
+++ src/main/java/com/FileConverter/demo/FileConverterApplication.java
@@ -0,0 +1,13 @@
+package com.FileConverter.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FileConverter/demo/FileConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FileConverter.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
